74eaa88b4d63aef032851c4e8710c5fa6a049a4e
LOPEZ2709 <diego.lopezm0405@gmail.com>
2026-05-18 18:32:26 -0500

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+import { Hero } from './components/hero/hero';
+import { Navbar } from './components/navbar/navbar';
+import { Projects } from './components/projects/projects';
+import { Services } from './components/services/services';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    About,
+    Contact,
+    Footer,
+    Hero,
+    Navbar,
+    Projects,
+    Services],
+      
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('PDIEGO');
