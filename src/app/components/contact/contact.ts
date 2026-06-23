d71dfd8605c838d61a4d2890688fe227e1339bd4
LOPEZ2709 <diego.lopezm0405@gmail.com>
2026-06-23 16:37:01 -0500

    23/06/2026

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
+import { NgComponentOutlet } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
